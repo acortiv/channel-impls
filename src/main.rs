1e964c097e011fd8210729036a3180a27d8d4e5b
acortiv <tacortiv@proton.me>
2026-02-28 14:37:45 -0700

    feat: one shot channel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::thread;
+
+use channels::Channel;
+
 fn main() {
-    println!("Hello, world!");
+    let channel = Channel::new();
+    let t = thread::current();
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("Hello World!");
+            t.unpark();
+        });
+        while !channel.is_ready() {
+            thread::park();
+        }
+        assert_eq!(channel.receive(), "Hello World!");
+    });
 }
